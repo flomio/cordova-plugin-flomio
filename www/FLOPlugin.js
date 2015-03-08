736426ba33b2153d70f3648b85effe460edf22cb
Flomio <john@flomio.com>
2015-03-07 20:57:30 -0500

    Update FLOPlugin.js

--- www/FLOPlugin.js
+++ www/FLOPlugin.js
@@ -1,7 +1,5 @@
-cordova.define("com.flomio.sdk.FLOPlugin", function(require, exports, module) {
-  window.echo = function(str, callback) {
-      cordova.exec(callback, function(err) {
-          callback('Flomio SDK plugin error occurred: ' + err);
-      }, "floPlugin", "webToSdkCommand", []);
-  };
-});
+window.echo = function(str, callback) {
+  cordova.exec(callback, function(err) {
+      callback('Flomio SDK plugin error occurred: ' + err);
+    }, "floPlugin", "webToSdkCommand", []);
+};
