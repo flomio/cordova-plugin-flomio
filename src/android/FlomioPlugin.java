04e6851168060a521ce34e75e4e2d6e1baff2f24
Darien Alvarez <dalvarez@kruger.com.ec>
2016-05-10 18:14:44 -0400

    modifying and adding methods

--- src/android/FlomioPlugin.java
+++ src/android/FlomioPlugin.java
@@ -9,18 +9,50 @@ public class FlomioPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("greet")) {
-
-            String name = data.getString(0);
-            String message = "FlomioPlugin, " + name;
-            callbackContext.success(message);
-
-            return true;
-
+        if (action.equals("init")) {
+            return init(data, callbackContext);
         } else {
-            
             return false;
-
         }
+    }
+
+    public boolean init(JSONArray data, CallbackContext callbackContext) {
+        String name = "Darien";
+        String message = "FlomioPlugin, " + name;
+        callbackContext.success(message);
+
+        return true;
+    }
+
+    public boolean setReaderSettings(JSONArray data, CallbackContext callbackContext) {
+        return false;
+    }
+
+    public boolean getReaderSettings(JSONArray data, CallbackContext callbackContext) {
+        return false;
+    }
+
+    public boolean selectReaderType(JSONArray data, CallbackContext callbackContext) {
+        return false;
+    }
+
+    public boolean startReader(JSONArray data, CallbackContext callbackContext) {
+        return false;
+    }
+
+    public boolean stopReader(JSONArray data, CallbackContext callbackContext) {
+        return false;
+    }
+
+    public boolean setReaderStatusChangeCallback(JSONArray data, CallbackContext callbackContext) {
+        return false;
+    }
+
+    public boolean sendApdu(JSONArray data, CallbackContext callbackContext) {
+        return false;
+    }
+
+    public boolean setFlobleConnectCallback(JSONArray data, CallbackContext callbackContext) {
+        return false;
     }
 }
